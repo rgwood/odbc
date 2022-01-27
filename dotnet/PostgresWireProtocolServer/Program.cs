26e8208cb4c80b9b310a6bf9578133804abc9269
Reilly Wood <reilly.wood@icloud.com>
2022-01-26 20:07:56 -0800

    Top-level statement

--- dotnet/PostgresWireProtocolServer/Program.cs
+++ dotnet/PostgresWireProtocolServer/Program.cs
@@ -1,11 +1,5 @@
-﻿namespace PostgresWireProtocolServer;
+﻿using PostgresWireProtocolServer;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("I am a Postgres Server");
-        var wireServer = new WireServer("127.0.0.1", 9876);
-        wireServer.StartListener();
-    }
-}
+Console.WriteLine("I am a Postgres Server");
+var wireServer = new WireServer("127.0.0.1", 9876);
+wireServer.StartListener();
