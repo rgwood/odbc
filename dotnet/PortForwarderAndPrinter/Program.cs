4f61a72696a7b70dc163db535b6fb26aebac0562
Reilly Wood <reilly.wood@icloud.com>
2022-01-27 21:29:59 -0800

    Port forwarder: implicit usings, top level statement

--- dotnet/PortForwarderAndPrinter/Program.cs
+++ dotnet/PortForwarderAndPrinter/Program.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using PortForwarderAndPrinter;
 
-namespace PortForwarderAndPrinter
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            var remoteIp = "127.0.0.1";
-            var remotePort = 5432;
-            var localPort = 9000;
-            Console.WriteLine($"Listening on port {localPort} and forwarding to {remoteIp}:{remotePort}");
-            var server = new Server("127.0.0.1", remotePort, localPort);
-            server.StartListener();
-        }
-    }
-}
+var remoteIp = "127.0.0.1";
+var remotePort = 5432;
+var localPort = 9000;
+Console.WriteLine($"Listening on port {localPort} and forwarding to {remoteIp}:{remotePort}");
+var server = new Server("127.0.0.1", remotePort, localPort);
+server.StartListener();
