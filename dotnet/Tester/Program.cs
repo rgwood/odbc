10af5af9637ef5cafae59b251ee1c6a07b83fcc7
Reilly Wood <reilly.wood@icloud.com>
2022-01-29 21:05:34 -0800

    Add test console app

---
+++ dotnet/Tester/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Npgsql;
+using PostgresWireProtocolServer;
+using Spectre.Console;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var cts = new CancellationTokenSource();
+
+        var wireServer = new WireServer("127.0.0.1", 5432, cts.Token);
+        var listenerTask = wireServer.StartListener();
+
+        var connString = "Host=localhost:5432;Username=mylogin;Password=mypass;Database=mydatabase";
+
+        try
+        {
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex.Demystify());
+        }
+
+        cts.Cancel();
+
+        try
+        {
+            await listenerTask;
+        }
+        catch (OperationCanceledException) // OK
+        {
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex.Demystify());
+        }
+    }
+}
